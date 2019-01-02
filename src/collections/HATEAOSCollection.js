96e3ee3c5f514402966b7efa0d55ecd3e01a8b2e
Julian Wolfe <juliancwolfe@gmail.com>
2019-01-01 21:02:59 -0500

    Adds support for adding custom embedded resources

--- src/collections/HATEAOSCollection.js
+++ src/collections/HATEAOSCollection.js
@@ -1,8 +1,8 @@
 import {Collection} from 'tramway-core-connection';
+import HATEOASItem from '../HATEOASItem';
 
-export default class HATEAOSCollection {
+export default class HATEAOSCollection extends HATEOASItem {
     _embedded = {};
-    _links = {};
 
     add(resource, entity) {
         if (!(resource in this._embedded)) {
@@ -21,34 +21,7 @@ export default class HATEAOSCollection {
         });
         
         return collection;
-    }
-
-    addLink(label, href) {
-        this._links[label] = {href};
-        return this;
-    }
-
-    getLinks() {
-        return this._links;
-    }
-
-    getLinksHeader() {
-        let links = {};
-
-        for (let label in this._links) {
-            links[label] = this._links[label].href;
-        }
-        
-        return links;
-    }
-
-    getLink(label) {
-        if (!(label in this._links)) {
-            return null;
-        }
-
-        return this._links[label].href;
-    }
+    }    
 
     generateLinks(urlGenerator, req) {
         this.addLink("self", urlGenerator.generateCurrent(req));
